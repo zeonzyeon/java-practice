a0c4d5f0d3a0abfd68df5a704139c01b0f06ac52
Jihyun Jeon <hyuneejj@naver.com>
2024-09-12 16:10:22 +0900

    10일차 수정

--- src/ch09/custom/Account.java
+++ src/ch09/custom/Account.java
@@ -1,11 +1,6 @@
 package ch09.custom;
-/**
- * 은행계좌 클래스
- * - 잔액 속성 (balance)
- * - 생성자
- * - 입금 기능
- * - 출금 기능 : 인출 money보다 잔액이 부족할경우 수동 예외 발생
- */
+
+// 은행 계좌 클래스
 public class Account {
     long balance;
 
@@ -18,9 +13,14 @@ public class Account {
     }
 
     void withdraw(long money) throws BalanceInsufficientException {
-        if (balance < money) {  // 출금 불가!
+        if (balance < money) { // 출금 불가!
             throw new BalanceInsufficientException("잔액이 부족합니다. 남은금액: " + balance);
         }
         balance -= money;
+    }
+
+    void withdrawRollback(long money) {
+        this.balance -= money;
+        System.out.println(money + "원이 롤백되었습니다.");
     }
 }
